d229c1071488faa66304608f148c757c07fb8d74
ljaviertovar <luisjavier_tovar@outlook.com>
2023-04-14 23:24:58 -0400

    add data and types

---
+++ src/types.d.ts
@@ -0,0 +1,14 @@
+export interface Product {
+	id: number
+	title: string
+	description: string
+	price: number
+	discountPercentage: number
+	rating: number
+	stock: number
+	brand: string
+	category: string
+	thumbnail: string
+	images: string[]
+	quantity?: number
+}

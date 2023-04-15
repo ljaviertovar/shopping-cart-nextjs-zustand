95be5aba1457268361eef094a88aec86efb3d921
ljaviertovar <luisjavier_tovar@outlook.com>
2023-04-14 23:24:03 -0400

    Initialize project

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx}',
+    './src/components/**/*.{js,ts,jsx,tsx}',
+    './src/app/**/*.{js,ts,jsx,tsx}',
+  ],
+  darkMode: false,
+  theme: {
+    extend: {
+    }
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
